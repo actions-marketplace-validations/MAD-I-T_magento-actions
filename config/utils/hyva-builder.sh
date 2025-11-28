3c95516bf7f28639ff04c870e8f7d85d98b7dcb1
seyuf <seyuf@hotmail.com>
2025-11-28 13:35:14 +0100

    auto detect tailwind node version

--- config/utils/hyva-builder.sh
+++ config/utils/hyva-builder.sh
@@ -8,13 +8,18 @@ echo "checking for hyva themes in - " $(pwd)
 for file in app/design/frontend/*/*; do
   if [ -d "$file/web/tailwind" ]
   then
+    PROJECT_NODE_VERSION=$(grep -o '"node": *"[^"]*"' package.json | sed 's/.*"node": *"\([^"]*\)".*/\1/' | sed 's/[^0-9]*\([0-9]\+\).*/\1/')
+
     if [ $IS_NODE_SET = 0 ]
     then
       curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.1/install.sh | bash
       export NVM_DIR="$HOME/.nvm"
       [ -s "$NVM_DIR/nvm.sh"  ] &&. "$NVM_DIR/nvm.sh"
-      nvm install $INPUT_NODE_VERSION
+      nvm install $PROJECT_NODE_VERSION
       IS_NODE_SET=1
+    else
+      nvm install $PROJECT_NODE_VERSION
+      nvm use $PROJECT_NODE_VERSION
     fi
     mkdir -p "$file/web/css/"
     npm --prefix "$file/web/tailwind" ci
